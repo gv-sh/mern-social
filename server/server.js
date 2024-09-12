5b37b6667802df40a29cb7d1151d8c6431afe945
gv-sh <108992402+gv-sh@users.noreply.github.com>
2024-09-12 13:04:50 +1000

    fix mongo error

--- server/server.js
+++ server/server.js
@@ -2,16 +2,22 @@ import config from './../config/config'
 import app from './express'
 import mongoose from 'mongoose'
 
-// Connection URL
-mongoose.Promise = global.Promise
-mongoose.connect(config.mongoUri, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true, useFindAndModify: false })
-mongoose.connection.on('error', () => {
-  throw new Error(`unable to connect to database: ${config.mongoUri}`)
-})
+const connectWithRetry = () => {
+  console.log('MongoDB connection with retry')
+  mongoose.connect(config.mongoUri, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true, useFindAndModify: false })
+    .then(() => {
+      console.log('MongoDB is connected')
+      app.listen(config.port, (err) => {
+        if (err) {
+          console.log(err)
+        }
+        console.info('Server started on port %s.', config.port)
+      })
+    })
+    .catch(err => {
+      console.log('MongoDB connection unsuccessful, retry after 5 seconds.', err)
+      setTimeout(connectWithRetry, 5000)
+    })
+}
 
-app.listen(config.port, (err) => {
-  if (err) {
-    console.log(err)
-  }
-  console.info('Server started on port %s.', config.port)
-})
+connectWithRetry()
