debded4131c82b03288ee295180bcacf69339f8f
gv-sh <108992402+gv-sh@users.noreply.github.com>
2024-09-12 12:28:23 +1000

    update server js

--- server/server.js
+++ server/server.js
@@ -1,17 +1,29 @@
-import config from './../config/config'
-import app from './express'
-import mongoose from 'mongoose'
+import config from './../config/config';
+import app from './express';
+import mongoose from 'mongoose';
 
-// Connection URL
-mongoose.Promise = global.Promise
-mongoose.connect(config.mongoUri, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true, useFindAndModify: false })
-mongoose.connection.on('error', () => {
-  throw new Error(`unable to connect to database: ${config.mongoUri}`)
-})
+mongoose.Promise = global.Promise;
+
+const connectWithRetry = () => {
+  console.log('MongoDB connection with retry');
+  mongoose.connect(config.mongoUri, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false
+  }).then(() => {
+    console.log('MongoDB is connected');
+  }).catch(err => {
+    console.log('MongoDB connection unsuccessful, retrying in 5 seconds...', err);
+    setTimeout(connectWithRetry, 5000);
+  });
+};
+
+connectWithRetry();
 
 app.listen(config.port, (err) => {
   if (err) {
-    console.log(err)
+    console.log(err);
   }
-  console.info('Server started on port %s.', config.port)
-})
+  console.info('Server started on port %s.', config.port);
+});
